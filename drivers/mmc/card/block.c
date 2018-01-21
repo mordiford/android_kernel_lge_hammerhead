25719ec383710c6379d5e850efd3f26a72dd1631
D. Andrei Măceș <dmaces@nd.edu>
2018-01-21 19:39:15 +0200

    Revert "mmc: add SECURE DISCARD CONFIG"
    This reverts commit bc19fe2c368c14587934b38ed242277842679ec5.

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -1385,7 +1385,6 @@ out:
 	return err ? 0 : 1;
 }
 
-#ifdef CONFIG_MMC_SECDISCARD
 static int mmc_blk_issue_secdiscard_rq(struct mmc_queue *mq,
 				       struct request *req)
 {
@@ -1451,7 +1450,7 @@ out:
 
 	return err ? 0 : 1;
 }
-#endif
+
 static int mmc_blk_issue_sanitize_rq(struct mmc_queue *mq,
 				      struct request *req)
 {
@@ -2760,7 +2759,6 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 		/* complete ongoing async transfer before issuing discard */
 		if (card->host->areq)
 			mmc_blk_issue_rw_rq(mq, NULL);
-#ifdef CONFIG_MMC_SECDISCARD
 		if (req->cmd_flags & REQ_SECURE &&
 			!(card->quirks & MMC_QUIRK_SEC_ERASE_TRIM_BROKEN)) {
 			ret = mmc_blk_issue_secdiscard_rq(mq, req);
@@ -2778,11 +2776,8 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 				}
 			}
 		} else {
-#endif
 			ret = mmc_blk_issue_discard_rq(mq, req);
-#ifdef CONFIG_MMC_SECDISCARD
 		}
-#endif
 	} else if (req && req->cmd_flags & REQ_FLUSH) {
 		/* complete ongoing async transfer before issuing flush */
 		if (card->host->areq)
